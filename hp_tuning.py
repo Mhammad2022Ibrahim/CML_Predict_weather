548c12fadb10104f0e74975786afaec58924f987
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2024-03-24 11:48:52 +0200

    update

--- hp_tuning.py
+++ hp_tuning.py
@@ -7,19 +7,22 @@ from utils_and_constants import PROCESSED_DATASET, get_hp_tuning_results, load_d
 
 
 def main():
+    # Load the data
     X, y = load_data(PROCESSED_DATASET)
     X_train, _, y_train, _ = train_test_split(X, y, random_state=1993)
 
+    # Define the model and hyperparameter search space
     model = RandomForestClassifier()
-    # Read the config file to define the hyperparameter search space
     param_grid = json.load(open("hp_config.json", "r"))
 
-    # Perform Grid Search Cross Validation on training data
+    # Perform GridSearchCV
     grid_search = GridSearchCV(model, param_grid, cv=5, n_jobs=1, verbose=2)
     grid_search.fit(X_train, y_train)
 
+    # Get the best hyperparameters
     best_params = grid_search.best_params_
 
+    # Print and save the best hyperparameters
     print("====================Best Hyperparameters==================")
     print(json.dumps(best_params, indent=2))
     print("==========================================================")
@@ -27,6 +30,7 @@ def main():
     with open("rfc_best_params.json", "w") as outfile:
         json.dump(best_params, outfile)
 
+    # Print and save the results of hyperparameter tuning
     markdown_table = get_hp_tuning_results(grid_search)
     with open("hp_tuning_results.md", "w") as markdown_file:
         markdown_file.write(markdown_table)
--- train.py
+++ train.py
@@ -1,39 +1,68 @@
+# import json
+
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+
+# from metrics_and_plots import plot_confusion_matrix, save_metrics
+# from model import evaluate_model, train_model
+# from utils_and_constants import PROCESSED_DATASET, TARGET_COLUMN
+
+
+# def load_data(file_path):
+#     data = pd.read_csv(file_path)
+#     X = data.drop(TARGET_COLUMN, axis=1)
+#     y = data[TARGET_COLUMN]
+#     return X, y
+
+
+# def main():
+#     X, y = load_data(PROCESSED_DATASET)
+    
+#     # Split data into training and test sets
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1993)
+
+#     # Train the model using the training set
+#     model = train_model(X_train, y_train)
+    
+#     # Calculate test set metrics
+#     metrics = evaluate_model(model, X_test, y_test)
+
+#     print("====================Test Set Metrics==================")
+#     print(json.dumps(metrics, indent=2))
+#     print("======================================================")
+
+#     # Save metrics into json file
+#     save_metrics(metrics)
+#     plot_confusion_matrix(model, X_test, y_test)
+
+
+# if __name__ == "__main__":
+#     main()
+
 import json
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-from metrics_and_plots import plot_confusion_matrix, save_metrics
+from metrics_and_plots import save_metrics, save_predictions, save_roc_curve
 from model import evaluate_model, train_model
-from utils_and_constants import PROCESSED_DATASET, TARGET_COLUMN
-
-
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    X = data.drop(TARGET_COLUMN, axis=1)
-    y = data[TARGET_COLUMN]
-    return X, y
+from sklearn.model_selection import train_test_split
+from utils_and_constants import PROCESSED_DATASET, load_data, load_hyperparameters
 
 
 def main():
     X, y = load_data(PROCESSED_DATASET)
-    
-    # Split data into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1993)
 
-    # Train the model using the training set
-    model = train_model(X_train, y_train)
-    
-    # Calculate test set metrics
-    metrics = evaluate_model(model, X_test, y_test)
+    # Load hyperparameters from the JSON file
+    hyperparameters = load_hyperparameters("rfc_best_params.json")
+    model = train_model(X_train, y_train, hyperparameters)
+    metrics, y_pred, y_pred_proba = evaluate_model(model, X_test, y_test)
 
     print("====================Test Set Metrics==================")
     print(json.dumps(metrics, indent=2))
     print("======================================================")
 
-    # Save metrics into json file
     save_metrics(metrics)
-    plot_confusion_matrix(model, X_test, y_test)
+    save_predictions(y_test, y_pred)
+    save_roc_curve(y_test, y_pred_proba)
 
 
 if __name__ == "__main__":
